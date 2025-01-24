34f9e72911c27cfdbd7c8cd7f9f21d3c62577568
Arjun Sharma <arjunsharma03.sa@gmail.com>
2025-01-24 23:48:50 +0530

    QuizLayout for zustand added

--- src/08.ZustandWith10Projects/src/Projects/9.QuizApp/components/QuizLayout.tsx
+++ src/08.ZustandWith10Projects/src/Projects/9.QuizApp/components/QuizLayout.tsx
@@ -1,11 +1,15 @@
-
+import Sidebar from "./Sidebar";
+import Question from "./Question";
 
 const QuizLayout = () => {
   return (
-    <div>
-      Quiz Layout
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <Question />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuizLayout
+export default QuizLayout;
